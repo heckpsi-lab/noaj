a771a69de98508cf175f47cfb6bd8a44dcd236e0
Delton Ding <dsh0416@gmail.com>
2016-03-17 22:51:19 +0800

    Rough revealing

--- src/frontend/yukkikaze.js
+++ src/frontend/yukkikaze.js
@@ -1,7 +1,14 @@
+WebSocket.prototype.isSending = function(){
+    return (this.bufferedAmount > 0);
+};
+
+WebSocket.prototype.isReceiving = false;
+
 var Ykk = {
     ajaxSockets: [],
+    ajaxUrl: '',
     ajax: function(param){
-        return new this.Ajax(param, this.ajaxSockets);
+        return new this.Ajax(param);
     },
     realtime: function(param){
         return new this.RealTime(param);
@@ -9,16 +16,34 @@ var Ykk = {
 };
 
 Ykk.Ajax  = (function (){
-    function Ajax(param, sockets) {
-        this.url = param.url;
+    function Ajax(param) {
         this.api = param.api;
         this.data = param.data;
         this.success = param.success;
         this.failure = param.failure;
-        this.sockets = sockets;
     }
-    Ajax.prototype.send = function() {
 
+    Ajax.prototype.send = function(param) {
+        var freeSocket = null;
+        for (var i in Ykk.ajaxSockets){
+            if (Ykk.ajaxSockets[i].bufferedAmount == 0 && !Ykk.ajaxSockets[i].isReceiving){
+                freeSocket = Ykk.ajaxSockets[i];
+                break;
+            }
+        }
+        if (freeSocket == null){
+            freeSocket = new WebSocket(Ykk.ajaxUrl);
+            Ykk.ajaxSockets.push(freeSocket);
+        }
+        freeSocket.send(JSON.stringify({api: param.api, data: param.data}));
+        freeSocket.isReceiving = true;
+        freeSocket.onmessage = function(event){
+            this.success(event.data);
+            if (Ykk.ajaxSockets.length > 1){
+                freeSocket.close();
+                Ykk.ajaxSockets.splice(Ykk.ajaxSockets.indexOf(freeSocket), 1);
+            }
+        }
     }
 }());
 
@@ -27,9 +52,6 @@ Ykk.RealTime = (function (){
         this.url = param.url;
         this.data = param.data;
         this.event = param.event;
-        this.isBusy = function(){
-            return (this.socket.bufferedAmount > 0);
-        }
     }
 
     RealTime.prototype.start = function() {
