83fb2abef052d1dfe3f5404606dafe93f2b167f6
Delton Ding <dsh0416@gmail.com>
2016-03-17 22:24:51 +0800

    Add realtime class

--- src/frontend/yukkikaze.js
+++ src/frontend/yukkikaze.js
@@ -27,9 +27,34 @@ Ykk.RealTime = (function (){
         this.url = param.url;
         this.data = param.data;
         this.event = param.event;
+        this.isBusy = function(){
+            return (this.socket.bufferedAmount > 0);
+        }
     }
 
     RealTime.prototype.start = function() {
+        this.socket = new WebSocket(this.url);
 
+        this.socket.onopen = function(ws){
+            ws.send(JSON.stringify(this.data));
+        };
+
+        this.socket.onmessage = function(event){
+            var data = JSON.parse(event.data);
+            for (var e in this.event){
+                if (e == data.event){
+                    this.event[e](data.data);
+                    break;
+                }
+            }
+        }
+    };
+
+    RealTime.prototype.send = function(data){
+        this.socket.send(JSON.stringify(data));
+    };
+
+    RealTime.prototype.stop = function(){
+        this.socket.close();
     };
 }());
